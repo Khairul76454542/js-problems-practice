3f1b9704c5d34a2c98d1ca46cbc316114543a570
khairul76454542 <khairul76454542@gmail.com>
2023-08-02 00:24:23 +0600

    same problem js

---
+++ problem.js
@@ -0,0 +1,19 @@
+const num1 = 10;
+const num2 = 20;
+const sum = num1 + num2;
+console.log(sum);
+
+// average of numbers
+const input1 = 75.25;
+const input2 = 65.8;
+const input3 = 35.45;
+const input4 = 99.5;
+const average = (input1 + input2 + input3 + input4) / 4;
+console.log(average);
+//  reminder of number
+const number = 128;
+const reminder = 128 % 5;
+console.log(reminder);
+const name = "jete jete pothe purnima rute chad utachilo gogone";
+const capName = name.toUpperCase();
+console.log(capName.toLowerCase());
